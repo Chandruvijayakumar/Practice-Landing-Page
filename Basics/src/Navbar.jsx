50697390a1e05ed544508fe950b6b688ab560887
CHANDRU VIJAYAKUMAR <chandru4395393@gmail.com>
2024-06-21 11:05:57 +0530

    Commit

--- Basics/src/Navbar.jsx
+++ Basics/src/Navbar.jsx
@@ -9,7 +9,7 @@ const Navbar = () => {
           <Link to="/">Home</Link>
         </li>
         <li>
-          <Link to="/about">About</Link>
+          <Link to="/about"></Link>
         </li>
         <li>
           <Link to="/portfolio">Portfolio</Link>
